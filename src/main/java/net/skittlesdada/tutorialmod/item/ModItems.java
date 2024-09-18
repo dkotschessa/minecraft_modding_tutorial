60a7a9205a1b30b8d7591dd8006213c64ed642ec
David Kotschessa <itzdavey@gmail.com>
2024-09-18 05:38:43 -0400

    tooltips

--- src/main/java/net/skittlesdada/tutorialmod/item/ModItems.java
+++ src/main/java/net/skittlesdada/tutorialmod/item/ModItems.java
@@ -1,6 +1,9 @@
 package net.skittlesdada.tutorialmod.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -8,6 +11,8 @@ import net.minecraftforge.registries.RegistryObject;
 import net.skittlesdada.tutorialmod.TutorialMod;
 import net.skittlesdada.tutorialmod.item.custom.ChiselItem;
 import net.skittlesdada.tutorialmod.item.custom.FuelItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
@@ -19,7 +24,13 @@ public class ModItems {
 
     public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
 
-    public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi", () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)));
+    public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi", () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI))
+    {
+        public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+            pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.kohlrabi"));
+            super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+        }
+    });
     public static final RegistryObject<Item> AURORA_ASHES = ITEMS.register("aurora_ashes",
             () -> new FuelItem(new Item.Properties(), 1200));
 
