e40efb38386b940838cf3432e1eb9a29600b5d50
David Kotschessa <itzdavey@gmail.com>
2024-09-21 15:47:07 -0400

    Revert "tags"
    This reverts commit bcbba66acfe196782bb3b63986ab7279305df993.

--- src/main/java/net/skittlesdada/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/skittlesdada/tutorialmod/block/custom/MagicBlock.java
@@ -16,7 +16,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.skittlesdada.tutorialmod.util.ModTags;
+import net.skittlesdada.tutorialmod.item.ModItems;
 
 import java.util.List;
 
@@ -37,7 +37,7 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level plevel, BlockPos pPos, BlockState pState, Entity pEntity) {
         if (pEntity instanceof ItemEntity itemEntity) {
-            if (isValidItem(itemEntity.getItem())){
+            if (itemEntity.getItem().getItem() == ModItems.RAW_ALEXANDRITE.get()) {
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
 
@@ -49,10 +49,6 @@ public class MagicBlock extends Block {
         super.stepOn(plevel, pPos, pState, pEntity);
 
         }
-
-    private boolean isValidItem(ItemStack item) {
-        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
-    }
 
     public void appendHoverText(ItemStack pStack, Item.TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
         pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.magic_block.tooltip"));
